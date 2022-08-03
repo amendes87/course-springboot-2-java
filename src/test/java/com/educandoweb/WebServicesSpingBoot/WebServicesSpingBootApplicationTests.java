9aaffa7a5c1860ac374c84fe2e0b692387c8a2c3
Alberto Mendes <meechfreddy@gmail.com>
2022-08-03 16:39:58 -0100

    Project created

---
+++ src/test/java/com/educandoweb/WebServicesSpingBoot/WebServicesSpingBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.WebServicesSpingBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesSpingBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
